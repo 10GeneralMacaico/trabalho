5b3bbd4e9a6f40f24975725031e0513e5c85e001
gustavo max. <ra54667252855@acaonsfatima.org.br>
2025-09-18 05:59:24 -0300

    Update clientesController.js

---
+++ novo/src/controller/clientesController.js
@@ -0,0 +1,36 @@
+import * as cliente from "../repository/clientesRepository.js";
+import { Router } from 'express';
+
+const endpoints = Router();
+
+endpoints.get('/clientes', async (req, resp) => {
+    let registros = await cliente.pesquisarClientes();
+    resp.send(registros);
+})
+
+endpoints.post('/clientes/enviar', async (req, resp) => {
+    let novo = req.body;
+    let id = await cliente.inserirClientes(novo);
+    resp.send({ novoI: id });
+})
+
+endpoints.put('/clientes/alt/:id', async (req, resp) => {
+    let id = Number(req.params.id);
+    let novo = req.body;
+    await cliente.alterarClientes(id, novo);
+    resp.send();
+})
+
+endpoints.delete('/clientes/del/:id', async (req, resp) => {
+    let id = Number(req.params.id);
+    await cliente.deletarClientes(id);
+    resp.send();
+})
+
+endpoints.get('/clientes/:id', async (req, resp) => {
+    let id = Number(req.params.id);
+    let novo = await cliente.pesquisarClientesId(id);
+    resp.send(novo);
+})
+
+export default endpoints;
