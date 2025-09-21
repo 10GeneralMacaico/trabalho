9f9515a481ead9418decae4cf75745eae696494f
gustavo max. <ra54667252855@acaonsfatima.org.br>
2025-09-20 22:19:18 -0300

    Update filmesRepository.js

---
+++ novo/src/repository/filmesRepository.js
@@ -0,0 +1,44 @@
+import conection from "./connection.js";
+
+export async function pesquisarFilmes() {
+  const Or = `
+  select *from filmes
+  `;
+  const [registro] = await conection.query(Or);
+  return registro;
+}
+
+export async function inserirFilmes(novo) {
+  const Or = `
+    INSERT INTO filmes (titulo, diretor, genero, duracao, ano_lancamento, classificacao, bilheteria, descricao, criado_em, atualizado_em)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+  const [info] = await conection.query(Or, Object.values(novo));
+  return info.insertId;
+}
+
+export async function deletarFilmes(id) {
+  const Or = `
+  delete from filmes 
+    where id =?
+  `;
+const [info] = await conection.query, [id]);
+}
+
+export async function alterarFilmes(id, novo) {
+  const ordem = `
+    UPDATE filmes SET
+      titulo=?, diretor=?, genero=?, duracao=?, ano_lancamento=?, classificacao=?, bilheteria=?, descricao=?, criado_em=?, atualizado_em=?
+    WHERE id=?
+  `;
+  await conection.query(ordem, [...Object.values(novo), id]);
+}
+
+export async function pesquisarFilmesId(id) {
+  const Or = `
+  select *from filmes
+    where id =?
+  `
+  const [registro] = await conection.query("SELECT * FROM filmes WHERE id=?", [id]);
+  return registro;
+}
