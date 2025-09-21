f283f3073a6ab329694439d1705bf8381348a340
gustavo max. <ra54667252855@acaonsfatima.org.br>
2025-09-20 21:34:22 -0300

    Update hoteisController.js

--- novo/src/controller/hoteisController.js
+++ novo/src/controller/hoteisController.js
@@ -1,43 +1,44 @@
-import * as usuario from "../repository/hoteisRepository.js";
+import * as hoteis from "../repository/hoteisRepository.js";
 import { Router } from 'express';
 
 const endpoints = Router();
 
 endpoints.get('/hotel', async (req, resp) => {
-    let registros = await usuario.pesquisarHotel();
+    let registros = await hoteis.pesquisarHotel();
     resp.send(registros);
 })
 
-endpoints.get('/usuarios/filtro', async (req, resp) => {
+endpoints.get('/hotel/filtro', async (req, resp) => {
   let nome = req.query.nome;
-  let registros = await usuario.filtrarPorNomeU(nome);
+  let registros = await hoteis.filtrarPorNomeH(nome);
   resp.send(registros);
 })
 
 endpoints.post('/hotel/enviar', async (req, resp) => {
     let novo = req.body;
-    let id = await usuario.inserirHotel(novo);
+    let id = await hoteis.inserirHotel(novo);
     resp.send({ novoI: id });
 })
 
 endpoints.put('/hotel/alt/:id', async (req, resp) => {
     let id = Number(req.params.id);
     let novo = req.body;
-    await usuario.alterarHotel(id, novo);
+    await hoteis.alterarHotel(id, novo);
     resp.send();
 })
 
 endpoints.delete('/hotel/del/:id', async (req, resp) => {
     let id = Number(req.params.id);
-    await usuario.deletarHotel(id);
+    await hoteis.deletarHotel(id);
     resp.send();
 })
 
 endpoints.get('/hotel/:id', async (req, resp) => {
     let id = Number(req.params.id);
-    let novo = await usuario.pesquisarHotelId(id);
+    let novo = await hoteis.pesquisarHotelId(id);
     resp.send(novo);
 })
 
 export default endpoints;
+
 
