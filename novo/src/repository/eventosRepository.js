f142b738b1187a0f8726bb8516dc05312a00ee96
gustavo max. <ra54667252855@acaonsfatima.org.br>
2025-09-21 18:10:20 -0300

    Update eventosRepository.js

---
+++ novo/src/repository/eventosRepository.js
@@ -0,0 +1,53 @@
+import conection from "./connection.js";
+
+export async function pesquisarEventos() {
+  const Or =`
+  select *from eventos
+  `
+  const [registro] = await conection.query(Or);
+  return registro;
+}
+
+export async function inserirEventos(novo) {
+  const ordem = `
+    insert into eventos (nome, local, data, horario, organizador, capacidade, preco, descricao, criado_em, atualizado_em)
+    values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+  const [info] = await conection.query(ordem, Object.values(novo));
+  return info.insertId;
+}
+
+export async function deletarEventos(id) {
+  const Or =`
+  delete from eventos
+   where id =?
+  `
+  await conection.query(Or, [id]);
+}
+
+export async function alterarEventos(id, novo) {
+  const ordem = `
+    update eventos
+      set nome=?, 
+          local=?, 
+          data=?, 
+          horario=?, 
+          organizador=?, 
+          capacidade=?, 
+          preco=?, 
+          descricao=?, 
+          criado_em=?, 
+          atualizado_em=?
+    where id=?
+  `;
+  await conection.query(ordem, [...Object.values(novo), id]);
+}
+
+export async function pesquisarEventosId(id) {
+  const Or =`
+  select *from eventos
+   where id =?
+  `
+  const [registro] = await conection.query(Or, [id]);
+  return registro;
+}
