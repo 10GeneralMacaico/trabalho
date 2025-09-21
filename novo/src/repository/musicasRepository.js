a417100ad5a868876254af9b77f1c38e81746d1f
gustavo max. <ra54667252855@acaonsfatima.org.br>
2025-09-21 18:03:21 -0300

    Update musicasRepository.js

---
+++ novo/src/repository/musicasRepository.js
@@ -0,0 +1,53 @@
+import conection from "./connection.js";
+
+export async function pesquisarMusicas() {
+  const Or =`
+  select *from musicas
+  `
+  const [registro] = await conection.query(Or);
+  return registro;
+}
+
+export async function inserirMusicas(novo) {
+  const ordem = `
+    insert into musicas (titulo, artista, album, genero, duracao, ano_lancamento, gravadora, preco, criado_em, atualizado_em)
+    values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+  const [info] = await conection.query(ordem, Object.values(novo));
+  return info.insertId;
+}
+
+export async function deletarMusicas(id) {
+  const Or =`
+  delete from musicas
+    where id = ?
+  `
+  await conection.query(Or, [id]);
+}
+
+export async function alterarMusicas(id, novo) {
+  const ordem = `
+    update musicas
+      set titulo=?, 
+          artista=?, 
+          album=?, 
+          genero=?, 
+          duracao=?, 
+          ano_lancamento=?, 
+          gravadora=?, 
+          preco=?, 
+          criado_em=?, 
+          atualizado_em=?
+    where id=?
+  `;
+  await conection.query(ordem, [...Object.values(novo), id]);
+}
+
+export async function pesquisarMusicasId(id) {
+  const Or =`
+  select *from musicas
+    where id = ?
+  `
+  const [registro] = await conection.query(Or, [id]);
+  return registro;
+}
