1c8f2a15303fde5de78021637696afc637e26d8f
gustavo max. <ra54667252855@acaonsfatima.org.br>
2025-09-22 05:14:37 -0300

    Update pedidosController.js

--- novo/src/controller/pedidosController.js
+++ novo/src/controller/pedidosController.js
@@ -39,6 +39,29 @@ endpoints.get('/pedidos/:id', async (req, resp) => {
     resp.send(novo);
 })
 
+endpoints.put('/pedidos/:id', async (req, resp) => {
+    let id = Number(req.params.id);
+    let dados = req.body;
+    await cliente.alterarClientes(id, dados);
+    resp.status(200).json({ mensagem: "Cliente atualizado com sucesso", id, dados });
+})
+
+endpoints.delete('/pedidos/:id', async (req, resp) => {
+    let id = Number(req.params.id);
+    await cliente.deletarClientes(id);
+    resp.status(200).json({ mensagem: "Cliente removido", id });
+})
+
+endpoints.get('/pedidos/:id', async (req, resp) => {
+    let id = Number(req.params.id);
+    let clienteId = await cliente.pesquisarClientesId(id);
+    resp.status(200).json({ mensagem: "Busca concluída", resultado: clienteId });
+})
+
+endpoints.get('/pedidos/filtros', async (req, resp) => {
+    let nome = req.query.nome;
+    let registros = await cliente.filtrarPorNomeC(nome);
+    resp.status(200).json({ filtro: nome, total: registros.length, dados: registros });
+})
+
 export default endpoints;
-
-
