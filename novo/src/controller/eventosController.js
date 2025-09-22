c4418f428371dce36e9d7483ec37c4563cb68be1
gustavo max. <ra54667252855@acaonsfatima.org.br>
2025-09-22 05:32:17 -0300

    Update eventosController.js

--- novo/src/controller/eventosController.js
+++ novo/src/controller/eventosController.js
@@ -4,7 +4,7 @@ import { Router } from 'express';
 const endpoints = Router();
 
 endpoints.get('/evento', async (req, resp) => {
-    let registros = await event.pesquisarEvento();
+    let registros = await event.pesquisarEventos();
     resp.send(registros);
 })
 
@@ -16,51 +16,51 @@ endpoints.get('/evento/filtro', async (req, resp) => {
 
 endpoints.post('/evento/enviar', async (req, resp) => {
     let novo = req.body;
-    let id = await event.inserirEvento(novo);
+    let id = await event.inserirEventos(novo);
     resp.send({ novoI: id });
 })
 
 endpoints.put('/evento/alt/:id', async (req, resp) => {
     let id = Number(req.params.id);
     let novo = req.body;
-    await event.alterarEvento(id, novo);
+    await event.alterarEventos(id, novo);
     resp.send();
 })
 
 endpoints.delete('/evento/del/:id', async (req, resp) => {
     let id = Number(req.params.id);
-    await event.deletarEvento(id);
+    await event.deletarEventos(id);
     resp.send();
 })
 
 endpoints.get('/evento/:id', async (req, resp) => {
     let id = Number(req.params.id);
-    let novo = await event.pesquisarEventoId(id);
+    let novo = await event.pesquisarEventosId(id);
     resp.send(novo);
 })
 
 endpoints.put('/evento/:id', async (req, resp) => {
     let id = Number(req.params.id);
     let dados = req.body;
-    await cliente.alterarClientes(id, dados);
+    await cliente.alterarEventos(id, dados);
     resp.status(200).json({ mensagem: "Cliente atualizado com sucesso", id, dados });
 })
 
 endpoints.delete('/evento/:id', async (req, resp) => {
     let id = Number(req.params.id);
-    await cliente.deletarClientes(id);
+    await cliente.deletarEventos(id);
     resp.status(200).json({ mensagem: "Cliente removido", id });
 })
 
 endpoints.get('/evento/:id', async (req, resp) => {
     let id = Number(req.params.id);
-    let clienteId = await cliente.pesquisarClientesId(id);
+    let clienteId = await cliente.pesquisarEventosId(id);
     resp.status(200).json({ mensagem: "Busca concluída", resultado: clienteId });
 })
 
 endpoints.get('/evento/filtros', async (req, resp) => {
     let nome = req.query.nome;
-    let registros = await cliente.filtrarPorNomeC(nome);
+    let registros = await cliente.filtrarPorNomeE(nome);
     resp.status(200).json({ filtro: nome, total: registros.length, dados: registros });
 })
 
