3e33b88ba1d2d8c428b2b9359f45ffbf1730e026
gustavo max. <ra54667252855@acaonsfatima.org.br>
2025-09-22 05:25:54 -0300

    Update musicasController.js

--- novo/src/controller/musicasController.js
+++ novo/src/controller/musicasController.js
@@ -42,26 +42,27 @@ endpoints.get('/musicas/:id', async (req, resp) => {
 endpoints.put('/musicas/:id', async (req, resp) => {
     let id = Number(req.params.id);
     let dados = req.body;
-    await cliente.alterarClientes(id, dados);
+    await cliente.alterarMusicas(id, dados);
     resp.status(200).json({ mensagem: "Cliente atualizado com sucesso", id, dados });
 })
 
 endpoints.delete('/musicas/:id', async (req, resp) => {
     let id = Number(req.params.id);
-    await cliente.deletarClientes(id);
+    await cliente.deletarMusicas(id);
     resp.status(200).json({ mensagem: "Cliente removido", id });
 })
 
 endpoints.get('/musicas/:id', async (req, resp) => {
     let id = Number(req.params.id);
-    let clienteId = await cliente.pesquisarClientesId(id);
+    let clienteId = await cliente.pesquisarMusicasId(id);
     resp.status(200).json({ mensagem: "Busca concluída", resultado: clienteId });
 })
 
 endpoints.get('/musicas/filtros', async (req, resp) => {
     let nome = req.query.nome;
-    let registros = await cliente.filtrarPorNomeC(nome);
+    let registros = await cliente.filtrarPorNomeM(nome);
     resp.status(200).json({ filtro: nome, total: registros.length, dados: registros });
 })
 
 export default endpoints;
+
